fb0ef25498c8fc1207f71aceda49841e81126c2d
vanessacp22 <vanessacp0611@gmail.com>
2025-10-06 15:55:30 -0700

    AMP LINE
    I added padding, layering and an amp line

---
+++ main.js
@@ -0,0 +1,7 @@
+playButton.addEventListener('click', function() {
+      
+      const youtubeURL = "https://youtu.be/vAoOJLpYKJ0?si=VpFiQKmFVuugJpjG";
+      
+      window.open(youtubeURL, "_blank");
+    });
+    
